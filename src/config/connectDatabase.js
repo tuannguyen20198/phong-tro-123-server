7dfe6dc3417e2e252ecde1c51fdf66739ed9f102
tuannguyen20198 <lntt20198@gmail.com>
2023-10-25 21:21:50 +0700

    done ca

--- src/config/connectDatabase.js
+++ src/config/connectDatabase.js
@@ -4,16 +4,13 @@ require('dotenv').config()
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
-  logging: false,
-  dialectOptions: {
-    ssl: true,
-  },
-  // dialectOptions:{
-  //   ssl: {
-  //     require: true,
-  //     rejecUnauthorized: false
-  //   }
-  // }
+  logging: false,     
+  dialectOptions:{
+    ssl: {
+      require: true,
+      rejecUnauthorized: false
+    }
+  }
 });
 const connectDatabase = async () => {
   try {

df2bada14755aca64bdd715523d788d115125002
tuannguyen20198 <lntt20198@gmail.com>
2023-10-26 18:16:38 +0700

    done attributes

--- src/models/attribute.js
+++ src/models/attribute.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Attribute.hasOne(models.Attribute, {
+      Attribute.hasOne(models.Post, {
         foreignKey: "attributesId",
         as: "attributes",
       });

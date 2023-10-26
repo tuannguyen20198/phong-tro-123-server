a85da333ffa7c0cf34298587ea38537941d17df9
tuannguyen20198 <lntt20198@gmail.com>
2023-10-26 19:33:22 +0700

    done fix

--- src/controller/insert.js
+++ src/controller/insert.js
@@ -1,7 +1,7 @@
 import * as insertServices from "../services/insert";
 export const insert = async (req, res) => {
   try {
-    const response = await insertServices.insertService();
+    const response = await insertServices.createPricesAndArea();
     return res.status(200).json(response);
   } catch (error) {
     return res.status(500).json({
--- src/models/attribute.js
+++ src/models/attribute.js
@@ -13,6 +13,7 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: "attributesId",
         as: "attributes",
       });
+      
     }
   }
   Attribute.init(
